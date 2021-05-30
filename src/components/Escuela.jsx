d76fb93844ca1e0738700681a11bfb671809ab88
Valentina Arenas Lozano <Valentinaarenas2@gmail.com>
2021-05-30 13:48:39 -0500

    Se organiza la sección de rutas recomendadas

--- src/components/Escuela.jsx
+++ src/components/Escuela.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import escuela from '../assets/static/escuela.jpg';
+import imgCurso from '../assets/static/curso.png';
 
 class Escuela extends Component {
   render() {
@@ -8,13 +9,28 @@ class Escuela extends Component {
       <div className='escuela'>
         <div className='top'>
           <img src={escuela} alt='' />
-          <span>Escuela de</span>
-          <span>{name}</span>
+          <span>
+            Escuela de
+            <span>{name}</span>
+          </span>
         </div>
         <div className='cursos'>
-          <div className='curso' />
-          <div className='curso' />
-          <div className='curso' />
+          <div className='union' />
+          <div className='curso'>
+            <img src={imgCurso} alt='' />
+            <p>Curso de Sketch</p>
+          </div>
+          <div className='curso'>
+            <img src={imgCurso} alt='' />
+            <div>
+              <span>Tú estás aquí</span>
+              <p>Curso de Sketch</p>
+            </div>
+          </div>
+          <div className='curso'>
+            <img src={imgCurso} alt='' />
+            <p>Curso de Sketch</p>
+          </div>
         </div>
         <input type='button' value='conocer toda la escuela' className='boton-verde-grande' />
       </div>
